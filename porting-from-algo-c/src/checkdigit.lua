2ee5b8f3d393a37f8c33ad963504b2236495c8bb
nobi56 <44406129+nobi56@users.noreply.github.com>
2023-01-31 22:56:55 +0900

    Fix checkdigit.lua
    - remove something unnecessary in isISBN10
    - a little more (well?-)formatted

--- porting-from-algo-c/src/checkdigit.lua
+++ porting-from-algo-c/src/checkdigit.lua
@@ -15,28 +15,36 @@
 local function slicer(s)
 	return function (i)
 		local r = tonumber(s:sub(i,i))
-		assert(r ~= nil, ("ERROR: can't convert '%s' to number."):format(s:sub(i,i)))
+		assert(
+			type(r) == "number",
+			("ERROR: can't convert '%s' to number."):format(s:sub(i,i))
+		)
 		return r
 	end
 end
 
 local function isISBN10(s)
-	assert(type(s) == "string")
-	assert(#s == 10, "ERROR: ISBN-10 must be just 10-digit.")
+	assert(
+		type(s) == "string" and #s == 10,
+		"ERROR: ISBN-10 must be just a 10-digit string."
+	)
 
-	local t, f = { [0] = 0 }, slicer(s)
+	local t, f = {}, slicer(s)
+
 	for i=1,9 do t[i] = f(i) end
 	t[10] = (s:sub(10,10)=="X" or s:sub(10,10)=="x") and 10 or f(10)
 
-	for i=1,10 do t[i] = t[i] + t[i-1] end
-	for i=1,10 do t[i] = t[i] + t[i-1] end
+	for i=2,10 do t[i] = t[i] + t[i-1] end
+	for i=2,10 do t[i] = t[i] + t[i-1] end
 
 	return t[10] % 11 == 0
 end
 
 local function isISBN13(s)
-	assert(type(s) == "string")
-	assert(#s == 13, "ERROR: ISBN-13 must be just 13-digit.")
+	assert(
+		type(s) == "string" and #s == 13,
+		"ERROR: ISBN-13 must be just a 13-digit string."
+	)
 
 	local t, w, f = 0, 1, slicer(s)
 
