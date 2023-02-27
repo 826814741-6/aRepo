ab965003ecc1855588ff2836a3b7c7e301a0b46c
nobi56 <44406129+nobi56@users.noreply.github.com>
2023-02-27 13:16:04 +0900

    Add complex.lua

---
+++ porting-from-algo-c/examples/complex.lua
@@ -0,0 +1,232 @@
+--
+--	from src/complex.c
+--
+--	complex c_conv(double, double)		to	complexNumber, complexNumberS
+--	char * c_string(complex)		to	__tostring
+--
+--	double c_abs(complex)			to	:abs(, cnAbs)
+--	double c_arg(complex)			to	:arg(, cnArg)
+--
+--	complex c_conj(complex)			to	:conjugate / .conjugate
+--	complex c_add(complex, complex)		to	__add, cnAdd / .add
+--	complex c_sub(complex, complex)		to	__sub, cnSub / .sub
+--	complex c_mul(complex, complex)		to	__mul, cnMul / .mul
+--	complex c_div(complex, complex)		to	__div, cnDiv / .div
+--	complex c_pow(complex, complex)		to	__pow, cnPow / .pow
+--
+--	complex c_exp(complex)			to	:exp, cnExp / .exp
+--	complex c_log(complex)			to	:log, cnLog / .log
+--	complex c_sqrt(complex)			to	:sqrt, cnSqrt / .sqrt
+--
+--	complex c_sin(complex)			to	:sin, cnSin / .sin
+--	complex c_cos(complex)			to	:cos, cnCos / .cos
+--	complex c_tan(complex)			to	:tan, cnTan / .tan
+--	complex c_sinh(complex)			to	:sinh, cnSinh / .sinh
+--	complex c_cosh(complex)			to	:cosh, cnCosh / .cosh
+--	complex c_tanh(complex)			to	:tanh, cnTanh / .tanh
+--
+--							/ overloadWithInplaceMethods
+--
+
+local M = require 'complex'
+
+local complexNumber = M.complexNumber
+local complexNumberS = M.complexNumberS
+local cnAdd = M.cnAdd
+local cnSub = M.cnSub
+local cnMul = M.cnMul
+local cnDiv = M.cnDiv
+local cnPow = M.cnPow
+local cnExp = M.cnExp
+local cnLog = M.cnLog
+local cnSqrt = M.cnSqrt
+local cnSin = M.cnSin
+local cnCos = M.cnCos
+local cnTan = M.cnTan
+local cnSinh = M.cnSinh
+local cnCosh = M.cnCosh
+local cnTanh = M.cnTanh
+local overloadWithInplaceMethods = M.overloadWithInplaceMethods
+local cnAbs = M.cnAbs
+local cnArg = M.cnArg
+
+do
+	local a, b = complexNumber(1, -2), complexNumber(-2, 3)
+	local c, d = complexNumberS(1, -2), complexNumberS(-2, 3)
+
+	assert(a == complexNumber(1, -2))
+	assert(c == complexNumberS(1, -2))
+	assert(a == c and b == d)
+
+	assert(a:abs() == c:abs())
+	assert(a:abs() == cnAbs(a))
+	assert(a:arg() == c:arg())
+	assert(a:arg() == cnArg(a))
+	assert(a:conjugate():conjugate() == a)
+	assert(a:conjugate() == c:conjugate())
+
+	assert(a + b == cnAdd(c, d))
+	assert(a - b == cnSub(c, d))
+	assert(a * b == cnMul(c, d))
+	assert(a / b == cnDiv(c, d))
+	assert(a ^ b == cnPow(c, d))
+
+	assert(a:exp() == cnExp(c))
+	assert(a:log() == cnLog(c))
+	assert(a:sqrt() == cnSqrt(c))
+
+	assert(a:exp():log():exp() == cnExp(cnLog(cnExp(c))))
+	assert(a:log():exp():log() == cnLog(cnExp(cnLog(c))))
+
+	assert(a:sin() == cnSin(c))
+	assert(a:cos() == cnCos(c))
+	assert(a:tan() == cnTan(c))
+	assert(a:sinh() == cnSinh(c))
+	assert(a:cosh() == cnCosh(c))
+	assert(a:tanh() == cnTanh(c))
+
+	assert(a == c and b == d)
+	local r, i = a:get()
+	overloadWithInplaceMethods(a)
+	overloadWithInplaceMethods(c)
+	--
+	local t = complexNumberS(r, i):conjugate()
+	a.conjugate()
+	c.conjugate()
+	assert(t == a and t == c)
+	--
+	a:set(r, i)
+	c:set(r, i)
+	t = a + b
+	a.add(b)
+	c.add(d)
+	assert(t == a and t == c)
+	--
+	a:set(r, i)
+	c:set(r, i)
+	t = a - b
+	a.sub(b)
+	c.sub(d)
+	assert(t == a and t == c)
+	--
+	a:set(r, i)
+	c:set(r, i)
+	t = a * b
+	a.mul(b)
+	c.mul(d)
+	assert(t == a and t == c)
+	--
+	a:set(r, i)
+	c:set(r, i)
+	t = a / b
+	a.div(b)
+	c.div(d)
+	assert(t == a and t == c)
+	--
+	a:set(r, i)
+	c:set(r, i)
+	t = a ^ b
+	a.pow(b)
+	c.pow(d)
+	assert(t == a and t == c)
+	--
+	a:set(r, i)
+	c:set(r, i)
+	t = cnExp(a)
+	a.exp()
+	c.exp()
+	assert(t == a and t == c)
+	--
+	a:set(r, i)
+	c:set(r, i)
+	t = cnLog(a)
+	a.log()
+	c.log()
+	assert(t == a and t == c)
+	--
+	a:set(r, i)
+	c:set(r, i)
+	t = cnSqrt(a)
+	a.sqrt()
+	c.sqrt()
+	assert(t == a and t == c)
+	--
+	a:set(r, i)
+	c:set(r, i)
+	t = cnSin(a)
+	a.sin()
+	c.sin()
+	assert(t == a and t == c)
+	--
+	a:set(r, i)
+	c:set(r, i)
+	t = cnCos(a)
+	a.cos()
+	c.cos()
+	assert(t == a and t == c)
+	--
+	a:set(r, i)
+	c:set(r, i)
+	t = cnTan(a)
+	a.tan()
+	c.tan()
+	assert(t == a and t == c)
+	--
+	a:set(r, i)
+	c:set(r, i)
+	t = cnSinh(a)
+	a.sinh()
+	c.sinh()
+	assert(t == a and t == c)
+	--
+	a:set(r, i)
+	c:set(r, i)
+	t = cnCosh(a)
+	a.cosh()
+	c.cosh()
+	assert(t == a and t == c)
+	--
+	a:set(r, i)
+	c:set(r, i)
+	t = cnTanh(a)
+	a.tanh()
+	c.tanh()
+	assert(t == a and t == c)
+	--
+	a:set(r, i)
+	c:set(r, i)
+	a.log()
+	a.exp()
+	c.log()
+	c.exp()
+	assert(complexNumber(r, i):log():exp() == a)
+	assert(complexNumber(r, i):log():exp() == c)
+end
+
+local deg = math.deg
+function toPolarForm(c)
+	return ("%gxCis(%g(rad)) (%g(deg))"):format(c:abs(), c:arg(), deg(c:arg()))
+end
+
+function getReciprocal(c)
+	local r, i = c:conjugate():get()
+	if r == 0 and i == 0 then
+		io.stderr:write("Error: can't get the reciprocal of 0+0i\n")
+		return
+	end
+	local t = c:abs() * c:abs()
+	return complexNumber(r / t, i / t)
+end
+
+do
+	local a, b = complexNumber(1, 1), complexNumber(-1, 1)
+	local c, d = b:conjugate(), a:conjugate()
+
+	print(("%5s : %s"):format(a, toPolarForm(a)))
+	print(("%5s : %s"):format(b, toPolarForm(b)))
+	print(("%5s : %s"):format(c, toPolarForm(c)))
+	print(("%5s : %s"):format(d, toPolarForm(d)))
+
+	local e, f = complexNumberS(1, 0), complexNumberS(23, 45)
+	assert(e == f * getReciprocal(f))
+end
---
+++ porting-from-algo-c/src/complex.lua
@@ -0,0 +1,458 @@
+--
+--	from src/complex.c
+--
+--	complex c_conv(double, double)		to	complexNumber, complexNumberS
+--	char * c_string(complex)		to	__tostring
+--
+--	double c_abs(complex)			to	:abs(, cnAbs)
+--	double c_arg(complex)			to	:arg(, cnArg)
+--
+--	complex c_conj(complex)			to	:conjugate / .conjugate
+--	complex c_add(complex, complex)		to	__add, cnAdd / .add
+--	complex c_sub(complex, complex)		to	__sub, cnSub / .sub
+--	complex c_mul(complex, complex)		to	__mul, cnMul / .mul
+--	complex c_div(complex, complex)		to	__div, cnDiv / .div
+--	complex c_pow(complex, complex)		to	__pow, cnPow / .pow
+--
+--	complex c_exp(complex)			to	:exp, cnExp / .exp
+--	complex c_log(complex)			to	:log, cnLog / .log
+--	complex c_sqrt(complex)			to	:sqrt, cnSqrt / .sqrt
+--
+--	complex c_sin(complex)			to	:sin, cnSin / .sin
+--	complex c_cos(complex)			to	:cos, cnCos / .cos
+--	complex c_tan(complex)			to	:tan, cnTan / .tan
+--	complex c_sinh(complex)			to	:sinh, cnSinh / .sinh
+--	complex c_cosh(complex)			to	:cosh, cnCosh / .cosh
+--	complex c_tanh(complex)			to	:tanh, cnTanh / .tanh
+--
+--							/ overloadWithInplaceMethods
+--
+
+local abs = math.abs
+local atan = math.atan
+local cos = math.cos
+local exp = math.exp
+local log = math.log
+local sin = math.sin
+local sqrt = math.sqrt
+
+local SQRT05 = sqrt(0.5)
+
+local function complexNumber(real, imaginary)
+	local T = {
+		r = real ~= nil and real or 0,
+		i = imaginary ~= nil and imaginary or 0
+	}
+
+	setmetatable(T, {
+		__tostring = function (v)
+			return ("%g%+gi"):format(v.r, v.i)
+		end,
+		__eq = function (a, b)
+			return a.r == b.r and a.i == b.i
+		end,
+		__add = function (a, b)
+			return complexNumber(a.r + b.r, a.i + b.i)
+		end,
+		__sub = function (a, b)
+			return complexNumber(a.r - b.r, a.i - b.i)
+		end,
+		__mul = function (a, b)
+			return complexNumber(
+				a.r * b.r - a.i * b.i,
+				a.r * b.i + a.i * b.r
+			)
+		end,
+		__div = function (a, b)
+			if abs(b.r) >= abs(b.i) then
+				local w = b.i / b.r
+				local d = b.r + b.i * w
+				return complexNumber(
+					(a.r + a.i * w) / d,
+					(a.i - a.r * w) / d
+				)
+			else
+				local w = b.r / b.i
+				local d = b.r * w + b.i
+				return complexNumber(
+					(a.r * w + a.i) / d,
+					(a.i * w - a.r) / d
+				)
+			end
+		end,
+		__pow = function (a, b)
+			-- log(a)
+			local r, i = 0.5 * log(a.r * a.r + a.i * a.i), atan(a.i, a.r)
+			-- mul(b, log(a))
+			r, i = b.r * r - b.i * i, b.r * i + b.i * r
+			-- exp(mul(b, log(a)))
+			return complexNumber(exp(r) * cos(i), exp(r) * sin(i))
+		end
+	})
+
+	function T:exp()
+		return complexNumber(exp(T.r) * cos(T.i), exp(T.r) * sin(T.i))
+	end
+
+	function T:log()
+		return complexNumber(0.5 * log(T.r * T.r + T.i * T.i), atan(T.i, T.r))
+	end
+
+	function T:sqrt()
+		local w = sqrt(T:abs() + abs(T.r))
+		if T.r >= 0 then
+			return complexNumber(SQRT05 * w, SQRT05 * T.i / w)
+		else
+			return complexNumber(
+				SQRT05 * abs(T.i) / w,
+				T.i >= 0 and SQRT05 * w or -SQRT05 * w
+			)
+		end
+	end
+
+	function T:sin()
+		local e = exp(T.i)
+		local f = 1 / e
+		return complexNumber(0.5 * sin(T.r) * (e + f), 0.5 * cos(T.r) * (e - f))
+	end
+
+	function T:cos()
+		local e = exp(T.i)
+		local f = 1 / e
+		return complexNumber(0.5 * cos(T.r) * (f + e), 0.5 * sin(T.r) * (f - e))
+	end
+
+	function T:tan()
+		local e = exp(2 * T.i)
+		local f = 1 / e
+		local d = cos(2 * T.r) + 0.5 * (e + f)
+		return complexNumber(sin(2 * T.r) / d, 0.5 * (e - f) / d)
+	end
+
+	function T:sinh()
+		local e = exp(T.r)
+		local f = 1 / e
+		return complexNumber(0.5 * (e - f) * cos(T.i), 0.5 * (e + f) * sin(T.i))
+	end
+
+	function T:cosh()
+		local e = exp(T.r)
+		local f = 1 / e
+		return complexNumber(0.5 * (e + f) * cos(T.i), 0.5 * (e - f) * sin(T.i))
+	end
+
+	function T:tanh()
+		local e = exp(2 * T.r)
+		local f = 1 / e
+		local d = 0.5 * (e + f) + cos(2 * T.i)
+		return complexNumber(0.5 * (e - f) / d, sin(2 * T.i) / d)
+	end
+
+	function T:abs()
+		if T.r == 0 then return abs(T.i) end
+		if T.i == 0 then return abs(T.r) end
+		if abs(T.r) < abs(T.i) then
+			local t = T.r / T.i
+			return abs(T.i) * sqrt(1 + t * t)
+		else
+			local t = T.i / T.r
+			return abs(T.r) * sqrt(1 + t * t)
+		end
+	end
+
+	function T:arg()
+		return atan(T.i, T.r)
+	end
+
+	function T:conjugate()
+		return complexNumber(T.r, -T.i)
+	end
+
+	function T:get()
+		return T.r, T.i
+	end
+
+	function T:set(real, imaginary)
+		T.r, T.i = real, imaginary
+	end
+
+	return T
+end
+
+--------
+
+local function complexNumberS(real, imaginary)
+	local T = {
+		r = real ~= nil and real or 0,
+		i = imaginary ~= nil and imaginary or 0
+	}
+
+	setmetatable(T, {
+		__tostring = function (v)
+			return ("%g%+gi"):format(v.r, v.i)
+		end,
+		__eq = function (a, b)
+			return a.r == b.r and a.i == b.i
+		end
+	})
+
+	function T:abs()
+		if T.r == 0 then return abs(T.i) end
+		if T.i == 0 then return abs(T.r) end
+		if abs(T.r) < abs(T.i) then
+			local t = T.r / T.i
+			return abs(T.i) * sqrt(1 + t * t)
+		else
+			local t = T.i / T.r
+			return abs(T.r) * sqrt(1 + t * t)
+		end
+	end
+
+	function T:arg()
+		return atan(T.i, T.r)
+	end
+
+	function T:conjugate()
+		return complexNumberS(T.r, -T.i)
+	end
+
+	function T:get()
+		return T.r, T.i
+	end
+
+	function T:set(real, imaginary)
+		T.r, T.i = real, imaginary
+	end
+
+	return T
+end
+
+local function cnAdd(a, b)
+	return complexNumberS(a.r + b.r, a.i + b.i)
+end
+
+local function cnSub(a, b)
+	return complexNumberS(a.r - b.r, a.i - b.i)
+end
+
+local function cnMul(a, b)
+	return complexNumberS(a.r * b.r - a.i * b.i, a.r * b.i + a.i * b.r)
+end
+
+local function cnDiv(a, b)
+	if abs(b.r) >= abs(b.i) then
+		local w = b.i / b.r
+		local d = b.r + b.i * w
+		return complexNumberS((a.r + a.i * w) / d, (a.i - a.r * w) / d)
+	else
+		local w = b.r / b.i
+		local d = b.r * w + b.i
+		return complexNumberS((a.r * w + a.i) / d, (a.i * w - a.r) / d)
+	end
+end
+
+local function cnPow(a, b)
+	-- log(a)
+	local r, i = 0.5 * log(a.r * a.r + a.i * a.i), atan(a.i, a.r)
+	-- mul(b, log(a))
+	r, i = b.r * r - b.i * i, b.r * i + b.i * r
+	-- exp(mul(b, log(a)))
+	return complexNumberS(exp(r) * cos(i), exp(r) * sin(i))
+end
+
+local function cnExp(c)
+	return complexNumberS(exp(c.r) * cos(c.i), exp(c.r) * sin(c.i))
+end
+
+local function cnLog(c)
+	return complexNumberS(0.5 * log(c.r * c.r + c.i * c.i), atan(c.i, c.r))
+end
+
+local function cnSqrt(c)
+	local w = sqrt(c:abs() + abs(c.r))
+	if c.r >= 0 then
+		return complexNumberS(SQRT05 * w, SQRT05 * c.i / w)
+	else
+		return complexNumberS(
+			SQRT05 * abs(c.i) / w,
+			c.i >= 0 and SQRT05 * w or -SQRT05 * w
+		)
+	end
+end
+
+local function cnSin(c)
+	local e = exp(c.i)
+	local f = 1 / e
+	return complexNumberS(0.5 * sin(c.r) * (e + f), 0.5 * cos(c.r) * (e - f))
+end
+
+local function cnCos(c)
+	local e = exp(c.i)
+	local f = 1 / e
+	return complexNumberS(0.5 * cos(c.r) * (f + e), 0.5 * sin(c.r) * (f - e))
+end
+
+local function cnTan(c)
+	local e = exp(2 * c.i)
+	local f = 1 / e
+	local d = cos(2 * c.r) + 0.5 * (e + f)
+	return complexNumberS(sin(2 * c.r) / d, 0.5 * (e - f) / d)
+end
+
+local function cnSinh(c)
+	local e = exp(c.r)
+	local f = 1 / e
+	return complexNumberS(0.5 * (e - f) * cos(c.i), 0.5 * (e + f) * sin(c.i))
+end
+
+local function cnCosh(c)
+	local e = exp(c.r)
+	local f = 1 / e
+	return complexNumberS(0.5 * (e + f) * cos(c.i), 0.5 * (e - f) * sin(c.i))
+end
+
+local function cnTanh(c)
+	local e = exp(2 * c.r)
+	local f = 1 / e
+	local d = 0.5 * (e + f) + cos(2 * c.i)
+	return complexNumberS(0.5 * (e - f) / d, sin(2 * c.i) / d)
+end
+
+--------
+
+local function overloadWithInplaceMethods(c)
+	c.conjugate = function ()
+		c.i = -c.i
+	end
+
+	c.add = function (a)
+		c.r, c.i = c.r + a.r, c.i + a.i
+	end
+
+	c.sub = function (a)
+		c.r, c.i = c.r - a.r, c.i - a.i
+	end
+
+	c.mul = function (a)
+		c.r, c.i = c.r * a.r - c.i * a.i, c.r * a.i + c.i * a.r
+	end
+
+	c.div = function (a)
+		if abs(a.r) >= abs(a.i) then
+			local w = a.i / a.r
+			local d = a.r + a.i * w
+			c.r, c.i = (c.r + c.i * w) / d, (c.i - c.r * w) / d
+		else
+			local w = a.r / a.i
+			local d = a.r * w + a.i
+			c.r, c.i = (c.r * w + c.i) / d, (c.i * w - c.r) / d
+		end
+	end
+
+	c.pow = function (a)
+		-- log(c)
+		local r, i = 0.5 * log(c.r * c.r + c.i * c.i), atan(c.i, c.r)
+		-- mul(a, log(c))
+		r, i = a.r * r - a.i * i, a.r * i + a.i * r
+		-- exp(mul(a, log(c)))
+		c.r, c.i = exp(r) * cos(i), exp(r) * sin(i)
+	end
+
+	c.exp = function ()
+		c.r, c.i = exp(c.r) * cos(c.i), exp(c.r) * sin(c.i)
+	end
+
+	c.log = function ()
+		c.r, c.i = 0.5 * log(c.r * c.r + c.i * c.i), atan(c.i, c.r)
+	end
+
+	c.sqrt = function ()
+		local w = sqrt(c:abs() + abs(c.r))
+		if c.r >= 0 then
+			c.r, c.i = SQRT05 * w, SQRT05 * c.i / w
+		else
+			c.r = SQRT05 * abs(c.i) / w
+			c.i = c.i >= 0 and SQRT05 * w or -SQRT05 * w
+		end
+	end
+
+	c.sin = function ()
+		local e = exp(c.i)
+		local f = 1 / e
+		c.r, c.i = 0.5 * sin(c.r) * (e + f), 0.5 * cos(c.r) * (e - f)
+	end
+
+	c.cos = function ()
+		local e = exp(c.i)
+		local f = 1 / e
+		c.r, c.i = 0.5 * cos(c.r) * (f + e), 0.5 * sin(c.r) * (f - e)
+	end
+
+	c.tan = function ()
+		local e = exp(2 * c.i)
+		local f = 1 / e
+		local d = cos(2 * c.r) + 0.5 * (e + f)
+		c.r, c.i = sin(2 * c.r) / d, 0.5 * (e - f) / d
+	end
+
+	c.sinh = function ()
+		local e = exp(c.r)
+		local f = 1 / e
+		c.r, c.i = 0.5 * (e - f) * cos(c.i), 0.5 * (e + f) * sin(c.i)
+	end
+
+	c.cosh = function ()
+		local e = exp(c.r)
+		local f = 1 / e
+		c.r, c.i = 0.5 * (e + f) * cos(c.i), 0.5 * (e - f) * sin(c.i)
+	end
+
+	c.tanh = function ()
+		local e = exp(2 * c.r)
+		local f = 1 / e
+		local d = 0.5 * (e + f) + cos(2 * c.i)
+		c.r, c.i = 0.5 * (e - f) / d, sin(2 * c.i) / d
+	end
+end
+
+--------
+
+local function cnAbs(c)
+	if c.r == 0 then return abs(c.i) end
+	if c.i == 0 then return abs(c.r) end
+	if abs(c.r) < abs(c.i) then
+		local t = c.r / c.i
+		return abs(c.i) * sqrt(1 + t * t)
+	else
+		local t = c.i / c.r
+		return abs(c.r) * sqrt(1 + t * t)
+	end
+end
+
+local function cnArg(c)
+	return atan(c.i, c.r)
+end
+
+--------
+
+return {
+	complexNumber = complexNumber,
+	complexNumberS = complexNumberS,
+	cnAdd = cnAdd,
+	cnSub = cnSub,
+	cnMul = cnMul,
+	cnDiv = cnDiv,
+	cnPow = cnPow,
+	cnExp = cnExp,
+	cnLog = cnLog,
+	cnSqrt = cnSqrt,
+	cnSin = cnSin,
+	cnCos = cnCos,
+	cnTan = cnTan,
+	cnSinh = cnSinh,
+	cnCosh = cnCosh,
+	cnTanh = cnTanh,
+	overloadWithInplaceMethods = overloadWithInplaceMethods,
+	cnAbs = cnAbs,
+	cnArg = cnArg
+}
