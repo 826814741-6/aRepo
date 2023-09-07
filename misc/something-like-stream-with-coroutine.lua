e9d8dacf8855fba372fe54c3f51402f1e45198cd
826814741_6 <44406129+826814741-6@users.noreply.github.com>
2023-09-07 17:41:13 +0900

    Add another toy script

---
+++ misc/something-like-stream-with-coroutine.lua
@@ -0,0 +1,191 @@
+--
+--	something-like-stream-with-coroutine.lua
+--
+--	> lua[jit] something-like-stream-with-coroutine.lua
+--	...
+--
+
+local co_create = coroutine.create
+local co_resume = coroutine.resume
+local co_yield = coroutine.yield
+local t_insert = table.insert
+local t_unpack = table.unpack ~= nil and table.unpack or unpack
+
+function coStuff(co)
+	local T = { co = co }
+
+	function T:skip(n)
+		n = n ~= nil and n or 0
+
+		for _=1,n do
+			co_resume(T.co)
+		end
+		return T, {}
+	end
+
+	-- something-like-take
+	function T:take(n)
+		n = n ~= nil and n or 0
+
+		local r = {}
+		for i=1,n do
+			local _, v = co_resume(T.co)
+			r[i] = v
+		end
+		return T, r
+	end
+
+	-- something-like-map
+	function T:map(n, f)
+		n = n ~= nil and n or 0
+		f = f ~= nil and f or function (e) return e end
+
+		local r = {}
+		for i=1,n do
+			local _, v = co_resume(T.co)
+			r[i] = f(v)
+		end
+		return T, r
+	end
+
+	-- something-like-filter
+	function T:filter(n, f)
+		n = n ~= nil and n or 0
+		f = f ~= nil and f or function (...) return true end
+
+		local r, i = {}, 0
+		repeat
+			local _, v = co_resume(T.co)
+			if f(i, v) then r[i], i = v, i + 1 end
+		until i > n
+		return T, r
+	end
+
+	return T
+end
+
+function extendWithBufferMethods(obj)
+	function obj:takeB(buffer, n)
+		local t, r = obj:take(n)
+		buffer:insert(r)
+		return t, r
+	end
+
+	function obj:mapB(buffer, n, f)
+		local t, r = obj:map(n, f)
+		buffer:insert(r)
+		return t, r
+	end
+
+	function obj:filterB(buffer, n, f)
+		local t, r = obj:filter(n, f)
+		buffer:insert(r)
+		return t, r
+	end
+
+	return obj
+end
+
+function makeBuffer()
+	local T = { buf = {} }
+
+	function T:insert(r)
+		local offset = #T.buf
+		for i,v in ipairs(r) do
+			T.buf[i+offset] = v
+		end
+	end
+
+	function T:get() return T.buf end
+
+	function T:reset() T.buf = {} end
+
+	return T
+end
+
+--
+
+-- 0, 1, 2, 3, 4, ...
+function seq(zero, one)
+	zero = zero ~= nil and zero or 0
+	one = one ~= nil and one or 1
+
+	return co_create(function ()
+		local i = zero
+		while true do
+			co_yield(i)
+			i = i + one
+		end
+	end)
+end
+
+-- 0, 1, 1, 2, 3, ...
+function fib(zero, one)
+	zero = zero ~= nil and zero or 0
+	one = one ~= nil and one or 1
+
+	return co_create(function ()
+		local a, b = zero, one
+		while true do
+			co_yield(a)
+			a, b = b, a + b
+		end
+	end)
+end
+
+-- 1, 2, 6, 24, 120, ...
+function fac(one)
+	one = one ~= nil and one or 1
+
+	return co_create(function ()
+		local n, acc = one, one
+		while true do
+			n, acc = n + one, acc * n
+			co_yield(acc)
+		end
+	end)
+end
+
+--
+
+do
+	function p(...)
+		local a, b = ...
+		print(t_unpack(b ~= nil and b or a:get()))
+	end
+
+	p(coStuff(seq()):take(10))
+	p(coStuff(seq()):skip(50):take(10))
+	p(coStuff(seq()):map(10, function (v) return v*v*v end))
+	p(coStuff(seq()):filter(10, function (_,v) if v%2==0 then return v end end))
+
+	print("--")
+
+	local co = coStuff(seq())
+	p(co:skip(5):take(5):skip(5):take(5))
+
+	local buf = makeBuffer()
+	co = extendWithBufferMethods(coStuff(seq()))
+	co:skip(5):takeB(buf,5):skip(5):takeB(buf,5)
+	p(buf)
+
+	buf:reset()
+	co = extendWithBufferMethods(coStuff(seq()))
+	co:skip(5):takeB(buf,5):skip(5):take(5):skip(5):takeB(buf,5)
+	p(buf)
+
+	print("--")
+
+	local hasBC, bc = pcall(require, 'bc')
+
+	local coA = coStuff(fib())
+	local coB = coStuff(fac(hasBC and bc.new(1) or 1))
+
+	p(coA:take(5))
+	p(coA:take(5))
+	p(coA:skip(30):take(1))
+
+	p(coB:take(5))
+	p(coB:take(5))
+	p(coB:skip(89):take(1))
+end
