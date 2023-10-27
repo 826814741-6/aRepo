b3e2685e77d70532d9d986fbcbdf052160b1e289
826814741_6 <44406129+826814741-6@users.noreply.github.com>
2023-10-27 20:59:21 +0900

    Add a bit

--- porting-from-algo-c/run-examples.sh
+++ porting-from-algo-c/run-examples.sh
@@ -18,6 +18,7 @@ HAXE=
 LUA=
 LUAJIT=
 PY=
+S7=
 
 #
 
@@ -33,6 +34,7 @@ error() {
 [ "$(command -v $LUA)" = "" ] && error "LUA"
 [ "$(command -v $LUAJIT)" = "" ] && error "LUAJIT"
 [ "$(command -v $PY)" = "" ] && error "PY"
+[ "$(command -v $S7)" = "" ] && error "S7"
 
 #
 
@@ -64,6 +66,10 @@ runPY() {
 	MICROPYPATH='src' PYTHONPATH='src' $PY src/${1}.py
 }
 
+runS7() {
+	$S7 examples/${1}.s7
+}
+
 #
 
 run() {
@@ -75,7 +81,7 @@ run() {
 	done
 }
 
-run acker AWK FTH LUA
+run acker AWK FTH LUA S7
 run atan LUA
 run binormal LUA
 run change AWK LUA
@@ -84,38 +90,38 @@ run ci LUA
 run combination AWK LUA
 run complex HAXE LUA
 run crnd LUA LUAJIT
-run cuberoot AWK FTH LUA LUAJIT
+run cuberoot AWK FTH LUA LUAJIT S7
 run dayweek AWK LUA
-run e AWK DMD FTH HAXE LUA
+run e AWK DMD FTH HAXE LUA S7
 run eulerian AWK FTH LUA
 run factorize LUA PY
 run fdist LUA
 run fft LUA
-run fib AWK FTH LUA
+run fib AWK FTH LUA S7
 run gamma AWK LUA
 run gcd AWK LUA
 run horner AWK LUA
-run hypot AWK FTH LUA
+run hypot AWK FTH LUA S7
 run isbn LUA
 run isbn13 LUA
 run komachi LUA
 run luhn LUA
 run machineepsilon DMD HAXE LUA PY
-run mccarthy AWK FTH LUA
+run mccarthy AWK FTH LUA S7
 run montecarlo LUA
 run moveblock AWK LUA
 run multiply AWK FTH LUA LUAJIT
 run normal AWK LUA
-run pi AWK FTH HAXE LUA
+run pi AWK FTH HAXE LUA S7
 run power AWK LUA LUAJIT
 run rand FTH LUA LUAJIT
 run randperm LUA
 run si LUA
-run sqrt AWK FTH LUA LUAJIT
+run sqrt AWK FTH LUA LUAJIT S7
 run stirling AWK FTH LUA
 run sum AWK LUA
 run swap AWK LUA
-run tarai AWK FTH LUA
+run tarai AWK FTH LUA S7
 run tdist LUA
 run totient AWK FTH LUA
 run whrnd LUA
