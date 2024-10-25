8693fee57500f092b8e7b7138f90e511e26c2c23
826814741_6 <44406129+826814741-6@users.noreply.github.com>
2024-10-26 01:43:51 +0900

    Consider the case where the return value contains nil

--- misc/function-wrapper-with-validator.lua
+++ misc/function-wrapper-with-validator.lua
@@ -12,7 +12,8 @@ function check(validators, target)
 	end
 end
 
-function wrapWithValidator(body, paramValidators, returnValidators)
+function wrapWithValidator(body, paramValidators, returnValidators, unpacker)
+	unpacker = unpacker ~= nil and unpacker or t_unpack
 	return setmetatable({}, {
 		__call = function (self, ...)
 			local arg = {...}
@@ -21,10 +22,17 @@ function wrapWithValidator(body, paramValidators, returnValidators)
 			local ret = {body(...)}
 			check(returnValidators, ret)
 
-			return t_unpack(ret)
+			return unpacker(ret)
 		end
 	})
 end
+--
+-- Note:
+-- If the return value contains nil, 't_unpack' may not be able to retrieve
+-- the result properly, depending on the version of Lua. Therefore, in such
+-- cases, please pass your own 'unpacker' as an argument.
+-- (see w4A and w4B below)
+--
 
 do
 	function isBool(v) return type(v) == "boolean" end
@@ -38,20 +46,31 @@ do
 	function f1(n, s, t) return n end
 	function f2() return true, false end
 	function f3(fh) end
-	function f4(n) return 0 end
+	function f4(n) return 0, nil, 1 end
 	function f5(n) return function (x, y) return "valid?" end end
 
 	local w1 = wrapWithValidator(f1, {isNum, isStr, isTbl}, {isNum})
 	local w2 = wrapWithValidator(f2, {}, {isBool, isBool})
 	local w3 = wrapWithValidator(f3, {isFh}, {})
-	local w4 = wrapWithValidator(f4, {isNumOrNil}, {isNumOrNil})
+	local w4A = wrapWithValidator(
+		f4,
+		{isNumOrNil},
+		{isNumOrNil, isNumOrNil, isNumOrNil}
+	)
+	local w4B = wrapWithValidator(
+		f4,
+		{isNumOrNil},
+		{isNumOrNil, isNumOrNil, isNumOrNil},
+		function (r) return r[1], r[2], r[3] end
+	)
 	local w5 = wrapWithValidator(f5, {isNum}, {isFun})
 	local w6 = wrapWithValidator(w5(0), {isNum, isNum}, {isStr})
 
 	print(w1(os.clock(), "1", {2}))
 	print(w2())
 	print(w3(io.stdout))
-	print(w4())
+	print("t_unpack:", w4A())
+	print("user unpacker:", w4B())
 	print(w5(0))
 	print(w6(1, 2))
 
